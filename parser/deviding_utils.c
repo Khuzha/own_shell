8d60b9c0dd87b3639ed77ef5d81a7dfc1c054cb7
Bracken Domitila <bdomitil@si-h2.kzn.21-school.ru>
2021-06-26 21:15:59 +0300

    fix misstakes in str pars

--- parser/deviding_utils.c
+++ parser/deviding_utils.c
@@ -34,27 +34,33 @@ t_deviders *get_deviders_list(char *str)
 	int			i;
 	int			pos;
 	t_deviders	*deviders;
+	int			qt_opened;
 
 	i = 0;
 	pos = 0;
+	qt_opened = -1;
 	deviders = NULL;
 	while (str[i] != '\0')
 	{
-		if (str[i] == '|')
+		if (qt_opened > 0 && find_open_quote(str, i, '\"') != qt_opened)
+			qt_opened = -1;
+		else
+			qt_opened = find_open_quote(str, i, '\"');
+		if (str[i] == '|' && qt_opened == -1)
 			lstadd_back_devide(&deviders, lstnew_devide(&pos, pipe_is_next));
-		else if (str[i] == '<')
+		else if (str[i] == '<' && qt_opened == -1)
 		{
 			if (str[i + 1] == '<')
 				i++;
 			else
 				lstadd_back_devide(&deviders, lstnew_devide(&pos, back_redir_is_next));
 		}
-		else if (str[i] == '>' && str[i + 1] == '>')
+		else if (str[i] == '>' && str[i + 1] == '>' && qt_opened == -1)
 		{
 			lstadd_back_devide(&deviders, lstnew_devide(&pos, double_redir_is_next));
 			i++;
 		}
-		else if (str[i] == '>')
+		else if (str[i] == '>' && qt_opened == -1)
 			lstadd_back_devide(&deviders, lstnew_devide(&pos, redir_is_next));
 		pos++;
 		i++;
